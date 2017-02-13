6c6e46274683820b1f5a148a62eb6188be3b037b
MengXJ <907564255@qq.com>
2017-02-13 13:53:07 +0800

    111

--- controller/ROMController.js
+++ controller/ROMController.js
@@ -1,6 +1,6 @@
 var app = angular.module('ROMApp',[]);
 app.controller('ROMCtrl',function($scope){
-	$scope.urltop = 'resources/head.html';
-	$scope.urltop_more = 'resources/head_more.html';
-	$scope.urlfooter = 'resources/footer.html';
+	$scope.urltop = 'https://yixiaoyi0522.github.io/lemengROM_picture/resources/head.html';
+	$scope.urltop_more = 'https://yixiaoyi0522.github.io/lemengROM_picture/resources/head_more.html';
+	$scope.urlfooter = 'https://yixiaoyi0522.github.io/lemengROM_picture/resources/footer.html';
 })
